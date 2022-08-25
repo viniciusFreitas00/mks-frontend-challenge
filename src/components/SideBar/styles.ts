7a5d99367937c8f29f003c62418b3c75cd337a87
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-08-24 23:00:32 -0300

    :hammer: change ovelay position

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -26,7 +26,7 @@ export const Container = styled.div<ContainerProps>`
 
 export const Overlay = styled.div`
   z-index: 1;
-  position: absolute;
+  position: fixed;
   left: 0;
   top: 0;
   right: 0;
